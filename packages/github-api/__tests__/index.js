4d67d95ad79debb54ccaa44f34904c699898301d
vivaxy <xyxuye2007@126.com>
2018-09-13 20:38:39 +0800

    chore:

--- packages/github-api/__tests__/index.js
+++ packages/github-api/__tests__/index.js
@@ -6,15 +6,28 @@
 const test = require('ava');
 const API = require('../index.js');
 
+const owner = 'Gemini-app';
+const repo = 'demo-notebook';
+const token = '';
+
 test('get files', async(t) => {
-  const owner = 'Gemini-app';
-  const repo = 'demo-notebook';
-  const token = '';
   const api = new API({ token });
   const ans = await api.getFiles({ owner, repo });
   t.is(ans.length, 3);
+});
+
+test('update file', async(t) => {
+  const api = new API({ token });
+  const ans = await api.getFiles({ owner, repo });
   const updateFile = ans[1];
-  const resp = await api.updateFile({ owner, repo, path: updateFile.path, sha: updateFile.sha, content: new Date().toString(), message: 'Commit with GeminiApp' });
-  console.log(resp);
-  t.pass();
+  const resp = await api.updateFile({
+    owner,
+    repo,
+    path: updateFile.path,
+    sha: updateFile.sha,
+    content: new Date().toString(),
+    message: 'Commit with GeminiApp',
+  });
+  t.is(typeof resp.content, 'object');
+  t.is(typeof resp.commit, 'object');
 });
